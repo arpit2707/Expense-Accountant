c14a1669e3e4f14d710f16b6c82353c6006778f7
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-05-01 00:56:36 +0530

    Added bcrypt to encrypt password

--- ExpenseTrackerBackend/controller/userC.js
+++ ExpenseTrackerBackend/controller/userC.js
@@ -1,66 +1,62 @@
 const path = require('path');
 const userList = require('../model/user');
 const fs = require('fs');
+const bcrypt = require('bcrypt');
 let indexHtmlFile = path.join(__dirname,'../../ExpenseTrackerFrontEnd/view/index.html');
 
+function isstringinvalid(string){
+    if(string == undefined || string.length===0){
+        return true;
+    }else{
+        return false;
+    }
+}
 exports.getIndex=(req,res,next)=>{
     res.sendFile(indexHtmlFile);
 }
 
-exports.addUser=(req,res,next)=>{
-    userList.create({
-        name:req.body.userName,
-        email:req.body.userEmail,
-        password:req.body.userPassword
-    }).then(user=>{
-        res.sendFile(indexHtmlFile);
-    })
-    .catch(errors=>{
-        fs.readFile(indexHtmlFile,'utf-8',(err ,data)=>{
-            if (err) {
-                // handle the error
-                res.status(500).send('Error reading index.html');
-              } else {
+exports.addUser=async (req,res,next)=>{
+    try{
+        const {name, email , password} = req.body;
 
-                const modifiedData = `${data}\n<p class="error">${errors}</p>`;
-                res.send(modifiedData);
-              }
-        })
+        if(isstringinvalid(name) || isstringinvalid(email) || isstringinvalid(password)){
+            return res.status(400).json({err:"Bad Paramters . Something is missing"})
+        }
+        const saltrounds =10;
+    bcrypt.hash(password,saltrounds,async(err,hash)=>{
+
+        console.log(err);
+        await userList.create({name, email, password:hash});
+        res.status(201).json({message:"successfully created new user"});
+
     })
+    }catch(errors){
+        res.status(500).json(errors);
+}
 }
 
-exports.loggedIn = (req,res,next)=>{
-    console.log("entered");
-    userList.findOne({where: {email:req.body.email}})
-    .then(result=>{
-        if(result.password===req.body.password){
-            fs.readFile(indexHtmlFile,'utf-8',(err,data)=>{
-                if(err){
-                    res.status(500).send("Error reading index.html");
-                }else{
-                    const modifiedData =`<script> alert('User Logged In Successfully')</script> ${data}`;
-                    res.send(modifiedData);
-                }
-            })  
-        }else{
-            fs.readFile(indexHtmlFile,'utf-8',(err,data)=>{
-                if(err){
-                    res.status(500).send("Error reading index.html");
-                }else{
-            const modifiedData = `${data}\n<p>"Error : User not authorized"</p>`;
-                res.status(401);
-                res.send(modifiedData);
-            }
-        })
-        }
-    }).catch(errors=>{
-        fs.readFile(indexHtmlFile,'utf-8',(err,data)=>{
-            if(err){
-                res.status(500).send("Error reading index.html");
+exports.loggedIn = async (req,res,next)=>{
+    try{
+        const {email , password} = req.body;
+
+        if(isstringinvalid(email) || isstringinvalid(password)){
+            return res.status(400).json({err:"Bad Paramters . Something is missing"});
+        } 
+    const user =await userList.findAll({where: {email}});
+    if(user.length>0){
+                bcrypt.compare(password, user[0].password,(err,result)=>{
+                if(result){
+                    console.log("entred password is same no error");
+                   res.status(200).json({success:true,message:"User logged in successfully"});
+                    } else{
+                        console.log("password is worng");
+                    return res.status(400).json({success:false,message:"Password is incorrect"});
+                    }
+                    })
             }else{
-                const modifiedData = `${data}\n<p class="error">${errors}</p>`;
-                res.send(modifiedData);
+                    return res.status(404).json({success:false, message:"User doesn't exist"});
             }
-        })
-    })
+    }catch(err){
+       return res.status(500).json({message:err,success:false});
+    }
 }
