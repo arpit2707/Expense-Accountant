f08b4d9960156e6a3ffb31898eace709079fcfdf
arpit2707 <arpitgaurav.goldi723@gmail.com>
2023-06-06 07:21:29 +0530

    Check

--- ExpenseTrackerFrontEnd/public/expenseAdd.js
+++ ExpenseTrackerFrontEnd/public/expenseAdd.js
@@ -71,7 +71,7 @@ function doPagination(data) {
   pagination.innerHTML = ``;
   if (data.hasPreviousPage) {
     const btn2 = document.createElement("button");
-    btn2.innerHTML = "Prev";
+    btn2.innerHTML = "Prevs";
     btn2.addEventListener("click", () => getProducts(data.prevPage));
     pagination.appendChild(btn2);
   }
